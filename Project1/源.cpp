89caa66b80fff7b776ab7345f374ecc6caeeef2d
3QJK <100700845+3QJK@users.noreply.github.com>
2022-03-06 21:44:42 +0800

    Add files via upload

---
+++ Project1/源.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int jjj = 0;
+	cin >> jjj;
+	cout << jjj << endl;
+	switch (jjj)
+	{
+	case 5:
+		cout << "fjca" << endl;
+		break;
+	};
+	system("pause");
+	return 0;
+}
